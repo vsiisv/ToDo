678eebde7686cb96b24bc8ef833b256e6f3af440
Vasilii <shores.freezer0u@icloud.com>
2023-06-01 18:15:54 +1000

    Save, Read and Delete data from CoreData

--- ToDo/Controllers/ToDoTableViewController.swift
+++ ToDo/Controllers/ToDoTableViewController.swift
@@ -6,28 +6,20 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoTableViewController: UITableViewController {
 	
 	private var array = [Item]()
-	
-	let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-//	let defaults = UserDefaults.standard
+	private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
 	// MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-		
 		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "list")
 		style()
-		
-//		if let array = defaults.array(forKey: "ToDoList") as? [Item] {
-//			self.array = array
-//		}
-		
 		loadItems()
-		
     }
 	
 	// MARK: - Methods
@@ -39,12 +31,14 @@ class ToDoTableViewController: UITableViewController {
 		let alert = UIAlertController(title: "Add List", message: nil, preferredStyle: .alert)
 		let action = UIAlertAction(title: "Add", style: .default) { actin in
 			if let text = textField.text, text != "" {
-				let item = Item(title: text, done: false)
+				
+				let item = Item(context: self.context)
+				item.title = text
+				item.done = false
+				
 				self.array.append(item)
 				self.tableView.insertRows(at: [IndexPath(row: self.array.count - 1, section: 0)], with: .automatic)
 			}
-			
-//			self.defaults.set(self.array, forKey: "ToDoList")
 			
 			self.saveItems()
 		}
@@ -57,27 +51,22 @@ class ToDoTableViewController: UITableViewController {
 		present(alert, animated: true)
 	}
 	
-	// Save item in plist
+	// Save item in CoreData
 	private func saveItems() {
-		let encoder = PropertyListEncoder()
 		do {
-			let data = try encoder.encode(array)
-			try data.write(to: dataFilePath!)
+			try context.save()
 		} catch {
-			print("ERRRRROOOOOOORRRRR - \(error)")
+			print("Error saving context \(error)")
 		}
 	}
 	
-	// Load Items from plist
+	// Load Items from CoreDate
 	private func loadItems() {
-		if let dataFilePath {
-			guard let data = try? Data(contentsOf: dataFilePath) else { return }
-			let decoder = PropertyListDecoder()
-			do {
-				array = try decoder.decode([Item].self, from: data)
-			} catch {
-				print("Error decoding item array \(error)")
-			}
+		let request: NSFetchRequest<Item> = Item.fetchRequest()
+		do {
+			array = try context.fetch(request)
+		} catch {
+			print("Error fetching data from context \(error)")
 		}
 	}
 
@@ -106,15 +95,15 @@ class ToDoTableViewController: UITableViewController {
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		tableView.deselectRow(at: indexPath, animated: true)
 		
-		array[indexPath.row].done = !array[indexPath.row].done
+		context.delete(array[indexPath.row])
+		array.remove(at: indexPath.row)
+		
+//		array[indexPath.row].done = !array[indexPath.row].done
 		
 		saveItems()
 
-		tableView.reloadRows(at: [indexPath], with: .automatic)
-	
-//		let accessory = tableView.cellForRow(at: indexPath)?.accessoryType
-//		tableView.cellForRow(at: indexPath)?.accessoryType = (accessory == .checkmark ? .none : .checkmark)
-		
+		tableView.deleteRows(at: [indexPath], with: .automatic)
+//		tableView.reloadRows(at: [indexPath], with: .automatic)
 	}
    
 }
